ebfde4e1f72b0c764dd97b9284b918b83d059f17
Hamidou Diallo <Dio.hamidou@gmail.com>
2019-11-14 13:39:39 -0500

    authentication, login, signup routes working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,10 +7,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+  
     user = User.find_by(username: params[:session][:username])
     if user&&user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      
       redirect_to user
     else
       flash[:errors] = "Could Not Find User, check your UN/PW or Signup"
